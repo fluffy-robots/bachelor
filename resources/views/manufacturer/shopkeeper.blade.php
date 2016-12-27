1ba9b5b0832dcc414fd033486d3f5f9aa920858f
= <=>
2016-12-28 00:10:10 +0100

    Martins rod

--- resources/views/manufacturer/shopkeeper.blade.php
+++ resources/views/manufacturer/shopkeeper.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
 Manufacturer Shopkeeper List
+<shopkeepers></shopkeepers>
 @endsection
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,29 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+
+Route::group(['middleware'=> 'auth', 'prefix' => 'v1'], function(){
+	Route::get('products', 'ProductController@index');
+	Route::post('products', 'ProductController@store');
+	Route::patch('products/{product}', 'ProductController@update');
+	Route::delete('products/{product}', 'ProductController@destroy');
+});
+
+// GET|HEAD  api/v1/products                
+// POST      api/v1/products                
+// GET|HEAD  api/v1/products/create         
+// GET|HEAD  api/v1/products/{product}      
+// PUT|PATCH api/v1/products/{product}      
+// DELETE    api/v1/products/{product}      
+// GET|HEAD  api/v1/products/{product}/edit
+
+
+// Sep 2013 1st Semester MMD
+// Sep 2014 3rd Semester MMD
+// May 2015 Dimension MMD
+// Sep 2015 1st Semester Web Dev
+// Sep 2016 3rd Semester Web Dev
+// Jan 2017 Dimension Web Dev
