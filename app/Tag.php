7cd7d3b8e7943537392cc55c2f7d900b1669ec89
= <=>
2016-12-28 01:40:24 +0100

    Relationships: User:tags,products

--- app/Tag.php
+++ app/Tag.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use App\User;
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    public function user(){
+    	return $this->belongsTo(User::class);
+    }
+    public function products(){
+    	return $this->belongsToMany(Product::class);
+    }
 }
--- database/migrations/2016_12_19_083800_create_tags_table.php
+++ database/migrations/2016_12_19_083800_create_tags_table.php
@@ -15,8 +15,13 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->string('name');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
         });
     }
 
