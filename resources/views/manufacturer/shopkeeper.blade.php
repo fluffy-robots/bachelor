61cbf2f736f5f23d11464177e128ce30cb07a5ee
mortensg <mooorten@gmail.com>
2016-12-28 10:22:12 +0100

    shopkeeeper done

--- resources/views/manufacturer/shopkeeper.blade.php
+++ resources/views/manufacturer/shopkeeper.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
-=======
-Manufacturer Shopkeeper List
->>>>>>> 7cd7d3b8e7943537392cc55c2f7d900b1669ec89
 <shopkeepers></shopkeepers>
 @endsection
